a2ca73fe59b65ecd06e8d47a95c10ac716efe1e1
paspartoshka <deolineww@gmail.com>
2025-04-22 14:49:24 +0300

    готовая программа

--- Lab5/src/main/java/org/example/Main.java
+++ Lab5/src/main/java/org/example/Main.java
@@ -1,7 +1,97 @@
 package org.example;
 
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+import java.util.Properties;
+import java.io.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@interface AutoInjectable{
+}
+
+
+interface SomeInterface{
+    void doSomething();
+}
+
+interface SomeOtherInterface{
+    void doSomething();
+}
+
+class SomeImpl implements SomeInterface
+{
+    public void doSomething()
+    {
+        System.out.println("A");
+    }
+}
+
+class OtherImpl implements SomeInterface {
+    public void doSomething()
+    {
+        System.out.println("B");
+    }
+}
+
+class SODoer implements SomeOtherInterface
+{
+    public void doSomething()
+    {
+        System.out.println("C");
+    }
+}
+
+class SomeBean {
+    @AutoInjectable
+    private SomeInterface field1;
+
+    @AutoInjectable
+    private SomeOtherInterface field2;
+
+    public void foo() {
+        field1.doSomething();
+        field2.doSomething();
+    }
+}
+
+class Injector {
+    private Properties properties;
+
+    public Injector(String fileName) {
+        properties = new Properties();
+        try {
+            properties.load(new FileInputStream(fileName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public <T> T inject(T obj) {
+        Field[] fields = obj.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(AutoInjectable.class)) {
+                String implClassName = properties.getProperty(field.getType().getName());
+                if (implClassName != null) {
+                    try {
+                        Class<?> implClass = Class.forName(implClassName);
+                        Object implInstance = implClass.getDeclaredConstructor().newInstance();
+                        field.setAccessible(true);
+                        field.set(obj, implInstance);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return obj;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Injector injector = new Injector("config.properties");
+        SomeBean bean = injector.inject(new SomeBean());
+        bean.foo();
     }
 }
